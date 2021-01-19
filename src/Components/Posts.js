3b77df02bb5a133594b99f71aa6989e4945537be
Kshitij Pandey <kshitijpandey32@gmail.com>
2021-01-19 12:47:54 +1100

    added scroll behavior for the content

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -21,6 +21,279 @@ const Posts = () => {
 					</div>
 				</div>
 			</div>
+
+
+			<div className="post">
+				<div className="post-heading">
+					<Link to="/post/1">What is a Virtual DOM?</Link>
+				</div>
+				<div className="post-snippet">
+					<p>Ever wondered how do the frameworks like React, Vue manage the DOM? Learn how these frameworks use Virtual DOM to...</p>
+				</div>
+				<div className="post-metadata">
+					<div className="publish-date">
+						<i className="fas fa-calendar-week"></i>
+						<p>
+							Sat, January 15 20201
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="post">
+				<div className="post-heading">
+					<Link to="/post/1">What is a Virtual DOM?</Link>
+				</div>
+				<div className="post-snippet">
+					<p>Ever wondered how do the frameworks like React, Vue manage the DOM? Learn how these frameworks use Virtual DOM to...</p>
+				</div>
+				<div className="post-metadata">
+					<div className="publish-date">
+						<i className="fas fa-calendar-week"></i>
+						<p>
+							Sat, January 15 20201
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="post">
+				<div className="post-heading">
+					<Link to="/post/1">What is a Virtual DOM?</Link>
+				</div>
+				<div className="post-snippet">
+					<p>Ever wondered how do the frameworks like React, Vue manage the DOM? Learn how these frameworks use Virtual DOM to...</p>
+				</div>
+				<div className="post-metadata">
+					<div className="publish-date">
+						<i className="fas fa-calendar-week"></i>
+						<p>
+							Sat, January 15 20201
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="post">
+				<div className="post-heading">
+					<Link to="/post/1">What is a Virtual DOM?</Link>
+				</div>
+				<div className="post-snippet">
+					<p>Ever wondered how do the frameworks like React, Vue manage the DOM? Learn how these frameworks use Virtual DOM to...</p>
+				</div>
+				<div className="post-metadata">
+					<div className="publish-date">
+						<i className="fas fa-calendar-week"></i>
+						<p>
+							Sat, January 15 20201
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="post">
+				<div className="post-heading">
+					<Link to="/post/1">What is a Virtual DOM?</Link>
+				</div>
+				<div className="post-snippet">
+					<p>Ever wondered how do the frameworks like React, Vue manage the DOM? Learn how these frameworks use Virtual DOM to...</p>
+				</div>
+				<div className="post-metadata">
+					<div className="publish-date">
+						<i className="fas fa-calendar-week"></i>
+						<p>
+							Sat, January 15 20201
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="post">
+				<div className="post-heading">
+					<Link to="/post/1">What is a Virtual DOM?</Link>
+				</div>
+				<div className="post-snippet">
+					<p>Ever wondered how do the frameworks like React, Vue manage the DOM? Learn how these frameworks use Virtual DOM to...</p>
+				</div>
+				<div className="post-metadata">
+					<div className="publish-date">
+						<i className="fas fa-calendar-week"></i>
+						<p>
+							Sat, January 15 20201
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="post">
+				<div className="post-heading">
+					<Link to="/post/1">What is a Virtual DOM?</Link>
+				</div>
+				<div className="post-snippet">
+					<p>Ever wondered how do the frameworks like React, Vue manage the DOM? Learn how these frameworks use Virtual DOM to...</p>
+				</div>
+				<div className="post-metadata">
+					<div className="publish-date">
+						<i className="fas fa-calendar-week"></i>
+						<p>
+							Sat, January 15 20201
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="post">
+				<div className="post-heading">
+					<Link to="/post/1">What is a Virtual DOM?</Link>
+				</div>
+				<div className="post-snippet">
+					<p>Ever wondered how do the frameworks like React, Vue manage the DOM? Learn how these frameworks use Virtual DOM to...</p>
+				</div>
+				<div className="post-metadata">
+					<div className="publish-date">
+						<i className="fas fa-calendar-week"></i>
+						<p>
+							Sat, January 15 20201
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="post">
+				<div className="post-heading">
+					<Link to="/post/1">What is a Virtual DOM?</Link>
+				</div>
+				<div className="post-snippet">
+					<p>Ever wondered how do the frameworks like React, Vue manage the DOM? Learn how these frameworks use Virtual DOM to...</p>
+				</div>
+				<div className="post-metadata">
+					<div className="publish-date">
+						<i className="fas fa-calendar-week"></i>
+						<p>
+							Sat, January 15 20201
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="post">
+				<div className="post-heading">
+					<Link to="/post/1">What is a Virtual DOM?</Link>
+				</div>
+				<div className="post-snippet">
+					<p>Ever wondered how do the frameworks like React, Vue manage the DOM? Learn how these frameworks use Virtual DOM to...</p>
+				</div>
+				<div className="post-metadata">
+					<div className="publish-date">
+						<i className="fas fa-calendar-week"></i>
+						<p>
+							Sat, January 15 20201
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="post">
+				<div className="post-heading">
+					<Link to="/post/1">What is a Virtual DOM?</Link>
+				</div>
+				<div className="post-snippet">
+					<p>Ever wondered how do the frameworks like React, Vue manage the DOM? Learn how these frameworks use Virtual DOM to...</p>
+				</div>
+				<div className="post-metadata">
+					<div className="publish-date">
+						<i className="fas fa-calendar-week"></i>
+						<p>
+							Sat, January 15 20201
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="post">
+				<div className="post-heading">
+					<Link to="/post/1">What is a Virtual DOM?</Link>
+				</div>
+				<div className="post-snippet">
+					<p>Ever wondered how do the frameworks like React, Vue manage the DOM? Learn how these frameworks use Virtual DOM to...</p>
+				</div>
+				<div className="post-metadata">
+					<div className="publish-date">
+						<i className="fas fa-calendar-week"></i>
+						<p>
+							Sat, January 15 20201
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="post">
+				<div className="post-heading">
+					<Link to="/post/1">What is a Virtual DOM?</Link>
+				</div>
+				<div className="post-snippet">
+					<p>Ever wondered how do the frameworks like React, Vue manage the DOM? Learn how these frameworks use Virtual DOM to...</p>
+				</div>
+				<div className="post-metadata">
+					<div className="publish-date">
+						<i className="fas fa-calendar-week"></i>
+						<p>
+							Sat, January 15 20201
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="post">
+				<div className="post-heading">
+					<Link to="/post/1">What is a Virtual DOM?</Link>
+				</div>
+				<div className="post-snippet">
+					<p>Ever wondered how do the frameworks like React, Vue manage the DOM? Learn how these frameworks use Virtual DOM to...</p>
+				</div>
+				<div className="post-metadata">
+					<div className="publish-date">
+						<i className="fas fa-calendar-week"></i>
+						<p>
+							Sat, January 15 20201
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="post">
+				<div className="post-heading">
+					<Link to="/post/1">What is a Virtual DOM?</Link>
+				</div>
+				<div className="post-snippet">
+					<p>Ever wondered how do the frameworks like React, Vue manage the DOM? Learn how these frameworks use Virtual DOM to...</p>
+				</div>
+				<div className="post-metadata">
+					<div className="publish-date">
+						<i className="fas fa-calendar-week"></i>
+						<p>
+							Sat, January 15 20201
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="post">
+				<div className="post-heading">
+					<Link to="/post/1">What is a Virtual DOM?</Link>
+				</div>
+				<div className="post-snippet">
+					<p>Ever wondered how do the frameworks like React, Vue manage the DOM? Learn how these frameworks use Virtual DOM to...</p>
+				</div>
+				<div className="post-metadata">
+					<div className="publish-date">
+						<i className="fas fa-calendar-week"></i>
+						<p>
+							Sat, January 15 20201
+						</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
